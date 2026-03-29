3b82752e91ec822a1758d7069dab317966ceddca
huynlt0101 <huynlt0101@gmail.com>
2026-03-30 03:50:31 +0700

    add order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const orderCtrl = require("../controllers/orderController");
+const { protect } = require("../middleware/auth"); 
+
+router.post("/", protect, orderCtrl.createOrder);
+router.get("/my", protect, orderCtrl.getMyOrders);
+
+
+router.get("/", protect, orderCtrl.getAllOrders);
+router.patch("/:id/status", protect, orderCtrl.updateOrderStatus);
+router.get("/total", protect, orderCtrl.getTotalOrders); 
+router.get("/stats/monthly", protect, orderCtrl.getMonthlyOrderStats);
+module.exports = router;
+
